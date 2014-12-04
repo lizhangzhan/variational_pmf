f0f83ff3490455b05ef76d10063f810aad624e50
Thomas Brouwer <tab43@cl.cam.ac.uk>
2014-12-04 14:44:44 +0000

    Fixed bug in code: -  to +. Also corrected F_q calculations. Converges correctly, but still just gets U,V to 0.

--- code/variational_pmf.py
+++ code/variational_pmf.py
@@ -22,18 +22,18 @@ class VariationalPMF:
 
 	def run(self,iterations):
 		# Initialize a (K,I) matrix U, S_U, (K,J) matrix V, S_V, (I,J) matrix R
-		#self.initialize()
-		self.U = numpy.array([
-			[ 2.20293498, -0.12139367,  0.67800237,  2.27199757],
-			[-1.85369011, -0.32077793, -0.11329014,  0.40277643]
-		])
-		self.S_U = [[1,1,1,1],[2,2,2,2]]
-		self.V = numpy.array([
-			[ 1.23343862, -3.47311768, -4.43899942,  1.18449626, -2.20931011],
-			[-0.07824337,  3.10808451, -1.37756161,  0.66224362,  0.04654989]
-		])
-		self.S_V = [[3,3,3,3,3],[2,2,2,2,2]]
-		self.R = self.X - numpy.dot(self.U.transpose(),self.V)
+		self.initialize()
+		#self.U = numpy.array([
+		#	[-0.44287367,  1.0638293,  -1.79817223,  0.08845199],
+	 	#	[-0.3400308,  -0.19817153,  1.20456161,  0.07377642]
+		#])
+		#self.S_U = [[1,1,1,1],[1,1,1,1]]
+		#self.V = numpy.array([
+		#	[ 1.09437084,  0.62430629,  0.59796201, -0.20876766,  1.39772961],
+		#	[-0.64444032, -0.90888236,  0.18876245, -0.4161538,  -0.48067803]
+		#])
+		#self.S_V = [[1,1,1,1,1],[1,1,1,1,1]]
+		#self.R = self.X - numpy.dot(self.U.transpose(),self.V)
 
 		# self.omega_U will store an array of tuples Mi, containing the indices
 		# j for which Mij is 1. Similarly for self.omega_V, Mj, indices i; and
@@ -41,17 +41,25 @@ class VariationalPMF:
 		self.compute_omega()
 
 		# Initialize the hyperparameters alpha_k, beta_k, tau
-		#self.update_hyperparameters()
-		self.alpha = [1,2]
-		self.beta = [3,2]
-		self.tau = 2
+		self.update_hyperparameters()
+		#self.alpha = [1,1]
+		#self.beta = [1,1]
+		#self.tau = 1
+
+		self.calc_statistics()
+		print self.F_q
 
 		# Then repeatedly: update U, update V, update hyperparams
 		for i in range(0,iterations):
-			#self.update_U()
-			#self.update_V()
+			self.update_U()
+			self.calc_statistics()
+			print "#",self.F_q
+
+			self.update_V()
+			self.calc_statistics()
+			print self.F_q
+
 			self.update_hyperparameters()
-			
 			self.calc_statistics()
 			print self.F_q
 
@@ -116,10 +124,63 @@ class VariationalPMF:
 		return
 
 
+	def update_U(self):
+		#print "U before: ",self.U
+		#print "V: ",self.V
+		#print "S_U before: ", self.S_U
+		#print "alpha: ",self.alpha
+		#print "tau: ",self.tau
+
+		for i in range(0,self.I):
+			for k in range(0,self.K):
+				old_U_ki = self.U[k][i]
+				self.S_U[k][i] = 1.0 / ((1.0/self.alpha[k]) + (1.0/self.tau) * sum([(self.V[k][j]**2+self.S_V[k][j]) for j in self.omega_I[i]]))
+				self.U[k][i] = self.S_U[k][i] * ((1.0/self.tau) * sum([(self.R[i][j]+self.U[k][i]*self.V[k][j])*self.V[k][j] for j in self.omega_I[i]]))
+				for j in self.omega_I[i]:
+					self.R[i][j] = self.R[i][j] - (self.U[k][i]-old_U_ki) * self.V[k][j]
+					
+		#print
+		#print "U: ", self.U
+		#print "U after: ",self.U
+		#print "S_U after: ", self.S_U
+
+		#print "R: ", self.R
+		#MSE = sum([sum([(self.X[i][j] - self.R[i][j])**2 for j in range(0,self.J)]) for i in range(0,self.I)])
+		#print "MSE: ",MSE
+
+		return
+
+
+	def update_V(self):
+		for j in range(0,self.J):
+			for k in range(0,self.K):
+				old_V_kj = self.V[k][j]
+				self.S_V[k][j] = 1.0 / ((1.0/self.beta[k]) + (1.0/self.tau) * sum([(self.U[k][i]**2+self.S_U[k][i]) for i in self.omega_J[j]]))
+				self.V[k][j] = self.S_V[k][j] * ((1.0/self.tau) * sum([(self.R[i][j]+self.V[k][j]*self.U[k][i])*self.U[k][i] for i in self.omega_J[j]]))
+				for i in self.omega_J[j]:
+					self.R[i][j] = self.R[i][j] - (self.V[k][j]-old_V_kj) * self.U[k][i]
+		
+		#print
+		#print "V: ", self.V
+		#print "S_V: ", self.S_V
+
+		#print "R: ", self.R
+
+		return
+
+
 	def update_hyperparameters(self):
+		#print "alpha before: ",self.alpha
+		#print "beta before: ",self.beta
+		#print "S_U: ",self.S_U
+		#print "S_V: ",self.S_V
+
 		for k in range(0,self.K):
 			self.alpha[k] = sum([(self.U[k][i])**2 + self.S_U[k][i] for i in range(0,self.I)]) / float(self.I)
 			self.beta[k] = sum([(self.V[k][j])**2 + self.S_V[k][j] for j in range(0,self.J)]) / float(self.J)
+
+		#print "alpha after: ",self.alpha
+		#print "beta before: ",self.beta
 
 		#print "Alpha: ", self.alpha
 		#print "Beta: ", self.beta
@@ -141,62 +202,24 @@ class VariationalPMF:
 		return
 
 
-	def update_U(self):
-		for i in range(0,self.I):
-			for k in range(0,self.K):
-				old_U_ki = self.U[k][i]
-				self.S_U[k][i] = 1.0 / ((1.0/self.alpha[k]) + (1.0/self.tau) * sum([(self.V[k][j]**2+self.S_V[k][j]) for j in self.omega_I[i]]))
-				self.U[k][i] = self.S_U[k][i] * ((1.0/self.tau) * sum([(self.R[i][j]-self.U[k][i]*self.V[k][j])*self.V[k][j] for j in self.omega_I[i]]))
-				for j in self.omega_I[i]:
-					self.R[i][j] = self.R[i][j] - (self.U[k][i]-old_U_ki) * self.V[k][j]
-					
-		#print
-		#print "U: ", self.U
-		#print "S_U: ", self.S_U
-
-		#print "R: ", self.R
-		#MSE = sum([sum([(self.X[i][j] - self.R[i][j])**2 for j in range(0,self.J)]) for i in range(0,self.I)])
-		#print "MSE: ",MSE
-
-		return
-
-
-	def update_V(self):
-		for j in range(0,self.J):
-			for k in range(0,self.K):
-				old_V_kj = self.V[k][j]
-				self.S_V[k][j] = 1.0 / ((1.0/self.beta[k]) + (1.0/self.tau) * sum([(self.U[k][i]**2+self.S_U[k][i]) for i in self.omega_J[j]]))
-				self.V[k][j] = self.S_V[k][j] * ((1.0/self.tau) * sum([(self.R[i][j]-self.V[k][j]*self.U[k][i])*self.U[k][i] for i in self.omega_J[j]]))
-				for i in self.omega_J[j]:
-					self.R[i][j] = self.R[i][j] - (self.V[k][j]-old_V_kj) * self.U[k][i]
-		
-		#print
-		#print "V: ", self.V
-		#print "S_V: ", self.S_V
-
-		#print "R: ", self.R
-
-		return
-
-
 	def calc_statistics(self):
 		# See Kim and Choi for these definitions
-		self.RMSE = sum([sum([self.R[i][j]**2 for j in range(0,self.J)])for i in range(0,self.I)]) / (self.I * self.J)
+		self.RMSE = sum([self.R[i][j]**2 for (i,j) in self.omega]) / float(len(self.omega))
 		F = 0.0
 		for i in range(0,self.I):
 			for j in range(0,self.J):
 				E_ij = self.R[i][j]**2 + sum([
 					self.U[k][i]**2 * self.S_V[k][j] + self.V[k][j]**2 * self.S_U[k][i] + self.S_U[k][i]*self.S_V[k][j]
 					for k in range(0,self.K)])
-				F_ij = -(self.tau/2.0)*E_ij - (1.0)/(2.0)*math.log(2*math.pi*self.tau) # natural log
+				F_ij = -1.0/(2.0*self.tau)*E_ij - (1.0)/(2.0)*math.log(2*math.pi*self.tau) # natural log
 				F += F_ij
 		for i in range(0,self.I):
 			for k in range(0,self.K):
-				F_U_ki = -1.0/(2.0*self.alpha[k]) * (self.U[k][i]**2 + self.S_U[k][i]) + math.log(self.S_U[k][i]*self.alpha[k])/2.0 + 1.0/2.0
+				F_U_ki = -1.0/(2.0*self.alpha[k]) * (self.U[k][i]**2 + self.S_U[k][i]) + math.log(self.S_U[k][i]/self.alpha[k])/2.0 + 1.0/2.0
 				F += F_U_ki
 		for j in range(0,self.J):
 			for k in range(0,self.K):
-				F_V_kj = -1.0/(2.0*self.beta[k]) * (self.V[k][j]**2 + self.S_V[k][j]) + math.log(self.S_V[k][j]*self.beta[k])/2.0 + 1.0/2.0
+				F_V_kj = -1.0/(2.0*self.beta[k]) * (self.V[k][j]**2 + self.S_V[k][j]) + math.log(self.S_V[k][j]/self.beta[k])/2.0 + 1.0/2.0
 				F += F_V_kj
 		self.F_q = F
 		return
@@ -204,49 +227,47 @@ class VariationalPMF:
 
 if __name__ == "__main__":
 	# Original matrices:
-	alpha = [1,2]
-	beta = [3,2]
-	tau = 2
-	#original_U = numpy.array([
-	#	[random.normalvariate(0,alpha[0]) for i in range(0,4)],
-	#	[random.normalvariate(0,alpha[1]) for i in range(0,4)]
-	#])
-	#original_V = numpy.array([
-	#	[random.normalvariate(0,beta[0]) for i in range(0,5)],
-	#	[random.normalvariate(0,beta[1]) for i in range(0,5)]
-	#])
-	#X = numpy.array([
-	#	[
-	#		random.normalvariate(element,tau)
-	#		for element in row
-	#	]
-	#	for row in numpy.dot(original_U.transpose(),original_V)
-	#])
+	I = 10
+	J = 20
+	K = 5
+	alpha = numpy.ones(I)
+	beta = numpy.ones(J)
+	tau = 1
+	
 	original_U = numpy.array([
-		[ 2.20293498, -0.12139367,  0.67800237,  2.27199757],
-		[-1.85369011, -0.32077793, -0.11329014,  0.40277643]
+		[random.normalvariate(0,math.sqrt(alpha[k])) for i in range(0,I)]
+		for k in range(0,K)
 	])
 	original_V = numpy.array([
-		[ 1.23343862, -3.47311768, -4.43899942,  1.18449626, -2.20931011],
-		[-0.07824337,  3.10808451, -1.37756161,  0.66224362,  0.04654989]
+		[random.normalvariate(0,math.sqrt(beta[k])) for j in range(0,J)]
+		for k in range(0,K)
 	])
 	X = numpy.array([
-		[  2.32431992, -11.7913782,   -7.74228389,   2.59442851,  -5.51584412],
- 		[  0.85785318,  -3.06949111,   0.72351582,   0.43589775,   0.13710034],
- 		[  1.51937854,   0.01188825,  -4.88318157,  -1.06202971,  -2.360093  ],
- 		[  1.28276217,  -5.64957929,  -9.51851513,   3.5672679,   -6.33356818]
+		[
+			random.normalvariate(element,math.sqrt(tau))
+			for element in row
+		]
+		for row in numpy.dot(original_U.transpose(),original_V)
 	])
+	#original_U = numpy.array([
+	#	[-0.44287367,  1.0638293,  -1.79817223,  0.08845199],
+ 	#	[-0.3400308,  -0.19817153,  1.20456161,  0.07377642]
+	#])
+	#original_V = numpy.array([
+	#	[ 1.09437084,  0.62430629,  0.59796201, -0.20876766,  1.39772961],
+	#	[-0.64444032, -0.90888236,  0.18876245, -0.4161538,  -0.48067803]
+	#])
+	#X = numpy.array([
+	#	[-0.53926979,  0.45974257,  0.71284042,  0.47260807,  1.03824097],
+ 	#	[ 2.48033453,  0.54513335, -0.77696111,  0.22562728,  0.79144723],
+ 	#	[-2.73801611,  0.27148848, -0.23274955,  0.44526388, -4.67589525],
+ 	#	[-0.14498833,  0.52374697,  0.95323515, -0.06717858,  2.16204996]
+	#])
 
-	M = numpy.array([
-		[1,	1,	1,	1,	1	],
-		[1, 1,	1,	1,	1	],
-		[1,	1,	1,	1,	1	],
-		[1,	1,	1,	1,	1	]
-	])
-	K = 2
+	M = numpy.ones([I,J])
 	
 	PMF = VariationalPMF(X,M,K)
-	PMF.run(100)
+	PMF.run(50)
 
 	X_predicted = numpy.dot(PMF.U.transpose(),PMF.V)
 
